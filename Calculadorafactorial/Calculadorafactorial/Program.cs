4e5a6f5b9087684867f829863a275c6109dcd1f8
Asarita02 <124800321+Asarita02@users.noreply.github.com>
2023-02-08 19:54:57 -0400

    Calculadorafactorial
    Practica 1 Programa0

---
+++ Calculadorafactorial/Calculadorafactorial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadorafactorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el número que desee para calcular su factorial: ");
+
+            int num = Convert.ToInt32(Console.ReadLine());
+            int resultado = 1;
+
+            for (int i = 1; i <= num; i++)
+            {
+                resultado = resultado * i;
+            }
+            Console.WriteLine("El factorial de " + num + " es " + resultado);
+            Console.ReadLine();
+        }
+    }
+}
