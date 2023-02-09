53b5650e820532fcc0f735e808004dbc142876a0
Asarita02 <124800321+Asarita02@users.noreply.github.com>
2023-02-08 20:31:29 -0400

    SerieFibonacci
    Ejercicio 1

---
+++ SerieFibonacci/SerieFibonacci/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerieFibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduzca el primer número de la serie Fibonacci: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Inntroduzca el límite definido: ");
+            int limitefibonacci = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Serie de Fibonacci desde " + num1 + " hasta " + limitefibonacci + ":");
+            Console.Write(num1 + " ");
+
+            int num2 = 0;
+            int resultado = 0;
+
+            while (resultado <= limitefibonacci)
+            {
+                resultado = num1 + num2;
+                num2 = num1;
+                num1 = resultado;
+
+                if (resultado > limitefibonacci)
+                {
+                    break;
+                }
+
+                Console.Write(resultado + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
